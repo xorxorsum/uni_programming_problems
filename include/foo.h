387abc51647f478be6bd0c9986e25992723def86
xorxorsum <126018490+xorxorsum@users.noreply.github.com>
2023-03-14 19:44:15 +0300

    Added Strassen functions prototypes

--- include/foo.h
+++ include/foo.h
@@ -18,6 +18,12 @@ Matrix MatrixSum(const Matrix a, const Matrix b);
 
 Matrix MatrixTransponate(const Matrix a);
 
+void StrassenSplit(Matrix a, Matrix a11, Matrix a12, Matrix a21, Matrix a22);
+
+Matrix StrassenCollect(Matrix a11, Matrix a12, Matrix a21, Matrix a22);
+
+Matrix Strassen(Matrix a, Matrix b);
+
 void FillMatrix(Matrix matrix);
 
 void FillMatrixFromFile(Matrix matrix, const char* filename);
